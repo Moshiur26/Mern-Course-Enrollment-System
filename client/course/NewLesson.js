35958c87954722bda1b6edc09506f0ff42759847
moshiur26 <moshiurrahmanb11@gmail.com>
2020-12-11 06:59:31 +0600

    .

--- client/course/NewLesson.js
+++ client/course/NewLesson.js
@@ -2,8 +2,17 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-type';
 import auth from '../auth/auth-helper';
 import { newLesson } from './api-course';
+import { Add } from '@material-ui/icons';
+import { Button, Dialog } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+    form: {
+        minWidth: 500
+    }
+}))
 
 export default function NewLesson(props) {
+    const classes = useStyles()
     const [open, setOpen] = useState(false);
     const [values, setValues] = useState({
        title: '',
@@ -11,6 +20,10 @@ export default function NewLesson(props) {
        resource_url: '' 
     });
     const jwt = auth.isAuthenticated()
+
+    const handleChange = name => event => {
+        setValues({...values, [name]: event.target.value})
+    }
     const handleClickOpen = () => {
         setOpen(true)
     }
@@ -40,7 +53,50 @@ export default function NewLesson(props) {
             })
     }
     return (<div>
-        
+        <Button arial-label="Add Lesson" color="primary" variant="contained" onClick={handleClickOpen}>
+            <Add/> New Lesson
+        </Button>
+        <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+            <div className={classes.form}>
+                <DialogTitle id="form-dialog-title">Add New Lesson</DialogTitle>
+                <DialogContent>
+                
+                <TextField
+                    margin="dense"
+                    label="Title"
+                    type="text"
+                    fullWidth
+                    value={values.title} onChange={handleChange('title')}
+                /><br/>
+                <TextField
+                    margin="dense"
+                    label="Content"
+                    type="text"
+                    multiline
+                    rows="5"
+                    fullWidth
+                    value={values.content} onChange={handleChange('content')}
+                /><br/>
+                <TextField
+                    margin="dense"
+                    label="Resource link"
+                    type="text"
+                    fullWidth
+                    value={values.resource_url} onChange={handleChange('resource_url')}
+                /><br/>
+                
+                </DialogContent>
+                
+                <DialogActions>
+                <Button onClick={handleClose} color="primary" variant="contained">
+                    Cancel
+                </Button>
+                <Button onClick={clickSubmit} color="secondary" variant="contained">
+                    Add
+                </Button>
+                </DialogActions>
+            </div>
+        </Dialog>
     </div>)
 
 };
